8bb463006fe005724ce8e78434bb0aa616e0ac4a
Andrew Ariotti <amariotti@gmail.com>
2012-10-12 15:27:00 -0600

    Created random number class
    RandNum.go(min,max)

---
+++ RandNum.java
@@ -0,0 +1,22 @@
+package ZombieApocalypse;
+
+import java.util.Random;
+
+/**
+ * Class containing the method to generate a random number
+ * @author amariotti
+ *
+ */
+public class RandNum {
+	
+	/**
+	 * Method to retrieve a random number below the int passed in
+	 * @param Maximum value that you'd like the number to be limited to
+	 * @return
+	 */
+	public static int go(int min, int max)
+	{
+		Random num = new Random();
+		return num.nextInt(max - min + 1) + min;
+	}
+}
--- main.java
+++ main.java
@@ -1,22 +1,33 @@
 package ZombieApocalypse;
 
-import java.security.PrivateKey;
 import java.util.ArrayList;
 
 public class main {
 
     public static void main(String[] args) {
-        ArrayList<Person> listOne = new ArrayList<Person>();
-
-        Person p1 = new Person("test",10,10,10,10,10,false,10);
-        listOne.add(p1) ;
-        listOne.add(p1) ;
-        listOne.add(p1) ;
-        listOne.add(p1) ;
-        System.out.print(listOne.size());
-        Encounter.encounter(listOne,listOne);
-        System.out.print(listOne.size());
+        ArrayList<Person> alZombie = new ArrayList<Person>();
+        ArrayList<Person> alPerson = new ArrayList<Person>();
         
-        //testing new push
+        for (int i = 0; i < 40; i++)
+        {
+        	int randomInfected = RandNum.go(1,2);
+        	
+        	if(randomInfected == 1)
+        	{
+        		Person zombie = new Person("zombie",100,100,100,100,100,true,100);
+        		alZombie.add(zombie);
+        	}
+        	else
+        	{
+        		Person person = new Person("person",100,100,100,100,100,false,100);
+        		alPerson.add(person);
+        	}
+		}
+        
+        System.out.println("Zombie Count: " + alZombie.size());
+        System.out.println("Person Count: " + alPerson.size());
+        
+        //Encounter.encounter(listOne,listOne);
+        //run an encounter
     }
 }
